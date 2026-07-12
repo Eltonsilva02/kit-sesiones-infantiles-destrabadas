b5250e6eebf4f3c0aa04a18f9a4f55a3434984a4
Eltonsilva02 <lokojunior062@gmail.com>
2026-07-12 18:24:03 -0300

    Atualização do código UTMify e novo Pixel do Facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,45 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className={`${inter.variable} ${playfair.variable}`}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2820618941647942');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2820618941647942&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.pixelId = "6a53c2ffd58c135c54567af0";
+              var a = document.createElement("script");
+              a.setAttribute("async", "");
+              a.setAttribute("defer", "");
+              a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+              document.head.appendChild(a);
+            `,
+          }}
+        />
+      </head>
       <body className="font-sans antialiased bg-white text-gray-900 min-h-screen">
         <main className="max-w-md mx-auto">{children}</main>
       </body>
